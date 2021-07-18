1639487a5aed2cf723a699137a5afe15f9cafe00
Philani Ncube <ncbphi001@gmail.com>
2021-07-18 14:09:46 +0200

    first commit with add to cart

---
+++ pages/product/[slug].js
@@ -0,0 +1,145 @@
+import React, { useContext } from 'react';
+import Image from 'next/image';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import {
+  Card,
+  Button,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+} from '@material-ui/core';
+import axios from 'axios';
+import useStyles from '../../utils/styles';
+import Layout from '../../components/Layout';
+import db from '../../utils/db';
+import Product from '../../models/Product';
+import { Store } from '../../utils/Store';
+
+const ProductPage = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const { dispatch } = useContext(Store);
+
+  const { product } = props;
+  console.log(product);
+
+  const classes = useStyles();
+
+  if (!product) {
+    return <Typography>Product Not Found</Typography>;
+  }
+
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock <= 0) {
+      window.alert('Sorry, product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+  };
+
+  return (
+    <Layout title={product.name} description={product.description}>
+      <div className={classes.section}>
+        <Head>
+          <title>{product.name} | Cape Gadgets</title>
+          <meta name="description" content={product.description} />
+        </Head>
+
+        <NextLink href="/" passHref>
+          <Link>Back To Products</Link>
+        </NextLink>
+      </div>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={500}
+            height={500}
+            layout="responsive"
+          />
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography>{product.name}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Category: {product.category}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Brand: {product.brand}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>
+                Rating: {product.rating} stars ({product.numReviews} reviews)
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Description: {product.description}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>R{product.price}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Status</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>
+                      {product.countInStock ? 'In Stock' : 'Unavailable'}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
+                  Add To Cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default ProductPage;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
+}
---
+++ utils/styles.js
@@ -0,0 +1,40 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#2C2E43',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+  toolbar: {
+    padding: '0',
+    margin: '0',
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.8rem',
+    padding: '0',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  price: {
+    width: '100%',
+    fontWeight: 700,
+  },
+  footer: {
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
+
+export default useStyles;
