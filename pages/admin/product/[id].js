841ab6e3150d0330c771172e42702e67535a5f78
Philani Ncube <ncbphi001@gmail.com>
2021-07-26 21:20:00 +0200

    update product

---
+++ pages/admin/product/[id].js
@@ -0,0 +1,418 @@
+import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
+import React, { useContext, useEffect } from 'react';
+import NextLink from 'next/link';
+import { Store } from '../../../utils/Store';
+import { Controller, useForm } from 'react-hook-form';
+import axios from 'axios';
+import {
+  Button,
+  Card,
+  CircularProgress,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import useStyles from '../../../utils/styles';
+import { getError } from '../../../utils/error';
+import Layout from '../../../components/Layout';
+import { useSnackbar } from 'notistack';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true, errorUpdate: '' };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false, errorUpdate: '' };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false, errorUpdate: action.payload };
+    default:
+      return state;
+  }
+}
+
+const ProductEdit = ({ params }) => {
+  const productId = params.id;
+  const { state } = useContext(Store);
+  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
+    loading: true,
+
+    error: '',
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const router = useRouter();
+  const classes = useStyles();
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (!userInfo) {
+      return router.push('/login');
+    } else {
+      const fetchData = async () => {
+        try {
+          dispatch({ type: 'FETCH_REQUEST' });
+          const { data } = await axios.get(`/api/admin/products/${productId}`, {
+            headers: { authorization: `Bearer ${userInfo.token}` },
+          });
+
+          dispatch({ type: 'FETCH_SUCCESS' });
+          setValue('name', data.name);
+          setValue('slug', data.slug);
+          setValue('price', data.price);
+          setValue('image', data.image);
+          setValue('category', data.category);
+          setValue('brand', data.brand);
+          setValue('countInStock', data.countInStock);
+          setValue('description', data.description);
+          setValue('sku', data.sku);
+        } catch (error) {
+          dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+        }
+      };
+      fetchData();
+    }
+  }, []);
+
+  const submitHandler = async ({
+    name,
+    slug,
+    price,
+    category,
+    image,
+    brand,
+    countInStock,
+    description,
+    sku,
+  }) => {
+    closeSnackbar();
+
+    try {
+      dispatch({ type: 'UPDATE_REQUEST' });
+      await axios.put(
+        `/api/admin/products/${productId}`,
+        {
+          name,
+          slug,
+          price,
+          category,
+          brand,
+          image,
+          countInStock,
+          description,
+          sku,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${userInfo.token}`,
+          },
+        },
+      );
+
+      dispatch({ type: 'UPDATE_SUCCESS' });
+      enqueueSnackbar('Product updated successfully', { variant: 'success' });
+      router.push('/admin/products');
+    } catch (error) {
+      dispatch({ type: 'UPDATE_FAIL', payload: getError(error) });
+      enqueueSnackbar(getError(error), { variant: 'error' });
+    }
+  };
+
+  return (
+    <Layout title={`Edit Product`}>
+      <Grid container spacing={1}>
+        <Grid item md={3} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <NextLink href="/admin/dashboard" passHref>
+                <ListItem button component="a">
+                  <ListItemText caption="Admin Dashboard">
+                    Admin Dashboard
+                  </ListItemText>
+                </ListItem>
+              </NextLink>
+              <NextLink href="/admin/orders" passHref>
+                <ListItem button component="a">
+                  <ListItemText caption="Orders">Orders</ListItemText>
+                </ListItem>
+              </NextLink>
+              <NextLink href="/admin/products" passHref>
+                <ListItem selected button component="a">
+                  <ListItemText primary="Products"></ListItemText>
+                </ListItem>
+              </NextLink>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={9} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography component="h1" variant="h1">
+                  Edit Product {productId}
+                </Typography>
+              </ListItem>
+
+              <ListItem>
+                {loading && <CircularProgress />}
+                {error && (
+                  <Typography className={classes.error}>{error}</Typography>
+                )}
+              </ListItem>
+              <ListItem>
+                <form
+                  onSubmit={handleSubmit(submitHandler)}
+                  className={classes.form}
+                >
+                  <List>
+                    <ListItem>
+                      <Controller
+                        name="name"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="name"
+                            label="Product Name"
+                            error={Boolean(errors.name)}
+                            helperText={
+                              errors.name ? 'Product name is required' : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="slug"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="slug"
+                            label="Product slug"
+                            error={Boolean(errors.slug)}
+                            helperText={
+                              errors.slug ? 'Product slug is required' : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="price"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="price"
+                            label="Price"
+                            error={Boolean(errors.price)}
+                            helperText={errors.name ? 'Price is required' : ''}
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="image"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="image"
+                            label="Image"
+                            error={Boolean(errors.image)}
+                            helperText={errors.image ? 'Image is required' : ''}
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="category"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="category"
+                            label="Category"
+                            error={Boolean(errors.category)}
+                            helperText={
+                              errors.category ? 'Category is required' : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="brand"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="brand"
+                            label="Brand"
+                            error={Boolean(errors.brand)}
+                            helperText={errors.brand ? 'Brand is required' : ''}
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="countInStock"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="countInStock"
+                            label="Count In Stock"
+                            error={Boolean(errors.countInStock)}
+                            helperText={
+                              errors.countInStock
+                                ? 'Count In Stock is required'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="description"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            multiline
+                            id="description"
+                            label="Description"
+                            error={Boolean(errors.description)}
+                            helperText={
+                              errors.description
+                                ? 'Description is required'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="sku"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="sku"
+                            label="sku"
+                            error={Boolean(errors.sku)}
+                            helperText={errors.sku ? 'sku is required' : ''}
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+
+                    <ListItem>
+                      <Button
+                        variant="contained"
+                        type="submit"
+                        fullWidth
+                        color="secondary"
+                      >
+                        Update
+                      </Button>
+                      {loadingUpdate && <CircularProgress />}
+                    </ListItem>
+                  </List>
+                </form>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps({ params }) {
+  return {
+    props: { params },
+  };
+}
+
+export default dynamic(() => Promise.resolve(ProductEdit), { ssr: false });
