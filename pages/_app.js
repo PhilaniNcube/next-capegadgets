52a0aada28691ac04ab2c5bc56af1cfb97b13de3
Philani Ncube <ncbphi001@gmail.com>
2021-08-11 22:44:37 +0200

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { SnackbarProvider } from 'notistack';
 import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-K3HRQQV' });
+  }, []);
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
