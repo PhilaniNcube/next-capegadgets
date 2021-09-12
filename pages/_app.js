e7b697231aff119da15b89f8a3f25e4d09d6e2af
Philani Ncube <ncbphi001@gmail.com>
2021-09-13 00:03:52 +0200

    add pageview event

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,18 @@ import { useEffect } from 'react';
 import FacebookPixel from '../components/FacebookPixel';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
+import { GTMPageView } from '../utils/gtm';
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => GTMPageView(url);
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
