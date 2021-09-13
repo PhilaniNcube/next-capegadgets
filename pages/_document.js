43051b1cd6fae7c1bb6bea4cc138a3f0b52fe9c8
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-13 09:28:22 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K3HRQQV');`}}></script>
+         
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -38,8 +34,7 @@ export default class MyDocument extends Document {
         <body>
         <NextScript />
         
-         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3HRQQV"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+       
           
           <noscript>
             <img
