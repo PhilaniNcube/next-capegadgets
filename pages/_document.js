de5de06771abb4210ef608b2848a4b6e68bf39a6
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-13 09:00:59 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,13 +11,12 @@ export default class MyDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K3HRQQV');`,
+              __html:
+                `(function(w,l){` +
+                `w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});` +
+                `})(window,'dataLayer');`,
             }}
-          ></script>
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -41,12 +40,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </Head>
         <body>
         <NextScript />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3HRQQV"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
+          
           <noscript>
             <img
               height={1}
