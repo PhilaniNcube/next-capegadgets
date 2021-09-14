8c4205a822d4018a6766086923c0bb0e177eb976
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-14 16:40:34 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,18 @@ import FacebookPixel from '../components/FacebookPixel';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 import { GTMPageView } from '../utils/gtm';
-import {useRouter} from 'next/router'
+import {useRouter} from 'next/router';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-   const Router = useRouter();
+   const router = useRouter();
    
    useEffect(() => {
-    Router.events.on('routeChangeComplete', GTMPageView)
+    router.events.on('routeChangeComplete', GTMPageView)
     return () => {
-      Router.events.off('routeChangeComplete', GTMPageView)
+      router.events.off('routeChangeComplete', GTMPageView)
     }
-  }, [Router.events])
+  }, [router.events])
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
@@ -27,6 +28,19 @@ function MyApp({ Component, pageProps }) {
      
     <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
       <StoreProvider>
+          {/* Google Tag Manager - Global base code */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-K3HRQQV');
+          `,
+        }}
+      />
         <FacebookPixel>
           <Component {...pageProps} />
         </FacebookPixel>
