e3358c8ccdc91c6707a568fb24ab60bf0c9c1c1a
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-14 14:30:06 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { ServerStyleSheets } from '@material-ui/core';
+import Script from 'next/script';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { FB_PIXEL_ID } from '../utils/fpixel';
 
@@ -11,6 +12,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-K3HRQQV');
+          `,
+        }}
+      />
          
           <script
             dangerouslySetInnerHTML={{
