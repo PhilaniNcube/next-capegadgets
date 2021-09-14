eb04718dd5ab18a95d8425a1d1b8162e915a4b93
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-14 11:27:06 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { SnackbarProvider } from 'notistack';
 import { useEffect } from 'react';
+import Script from 'next/script'
 import FacebookPixel from '../components/FacebookPixel';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
@@ -7,13 +8,12 @@ import { GTMPageView } from '../utils/gtm';
 import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const handleRouteChange = (url) => GTMPageView(url);
-    Router.events.on('routeChangeComplete', handleRouteChange);
+   useEffect(() => {
+    router.events.on('routeChangeComplete', GTMPageview)
     return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
+      Router.events.off('routeChangeComplete', GTMPageview)
+    }
+  }, [Router.events])
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
@@ -22,8 +22,19 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
   return (
- 
-   
+ <>
+     <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-K3HRQQV');
+          `,
+        }}
+      />
     <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
       <StoreProvider>
         <FacebookPixel>
@@ -31,6 +42,8 @@ function MyApp({ Component, pageProps }) {
         </FacebookPixel>
       </StoreProvider>
     </SnackbarProvider>
+
+</>
 
   );
 }
