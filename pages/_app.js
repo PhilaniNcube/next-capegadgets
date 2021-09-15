d58b5a9eae04f1b00645f531333a12f80c205830
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2021-09-15 14:49:26 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -42,14 +42,12 @@ function MyApp({ Component, pageProps }) {
         }}
       />
 
-   <Script
+        <Script
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            async  type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TU5Gj8"
-          `,
-        }}
-      />
+        async  
+        type="text/javascript" 
+        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TU5Gj8"
+        />
         <FacebookPixel>
           <Component {...pageProps} />
         </FacebookPixel>
